85d7c87438013cfa013f820cde09472e7bd4b80c
Norbert.Duchnowski <dnaid4k@gmail.com>
2023-09-07 18:38:04 +0200

    Big commit - check out the description

--- packages/cli/node_modules/local-api/dist/index.js
+++ packages/cli/node_modules/local-api/dist/index.js
@@ -4,10 +4,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.serve = void 0;
-const express_1 = __importDefault(require("express"));
-const serve = (port, filename, dir) => {
-    const app = (0, express_1.default)();
-    return new Promise((resolve, reject) => {
+var express_1 = __importDefault(require("express"));
+var http_proxy_middleware_1 = require("http-proxy-middleware");
+var serve = function (port, filename, dir) {
+    var app = express_1.default();
+    app.use('/', http_proxy_middleware_1.createProxyMiddleware({
+        target: 'http://127.0.0.1:3000',
+        ws: true,
+        logLevel: 'silent',
+    }));
+    return new Promise(function (resolve, reject) {
         app.listen(port, resolve).on('error', reject);
     });
 };
