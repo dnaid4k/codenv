36f7b653eb8ac633c2ccc0ecea79a45777b1cb09
Norbert.Duchnowski <dnaid4k@gmail.com>
2023-09-08 12:35:32 +0200

    production bundle - package link

--- packages/cli/node_modules/local-api/dist/index.js
+++ packages/cli/node_modules/local-api/dist/index.js
@@ -5,14 +5,19 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.serve = void 0;
 var express_1 = __importDefault(require("express"));
-var http_proxy_middleware_1 = require("http-proxy-middleware");
+var path_1 = __importDefault(require("path"));
 var serve = function (port, filename, dir) {
     var app = express_1.default();
-    app.use('/', http_proxy_middleware_1.createProxyMiddleware({
-        target: 'http://127.0.0.1:3000',
-        ws: true,
-        logLevel: 'silent',
-    }));
+    var packagePath = require.resolve('coen2/build/index.html');
+    app.use(express_1.default.static(path_1.default.dirname(packagePath)));
+    // app.use(
+    //   '/',
+    //   createProxyMiddleware({
+    //     target: 'http://127.0.0.1:3000',
+    //     ws: true,
+    //     logLevel: 'silent',
+    //   })
+    // );
     return new Promise(function (resolve, reject) {
         app.listen(port, resolve).on('error', reject);
     });
--- packages/cli/node_modules/local-api/src/index.ts
+++ packages/cli/node_modules/local-api/src/index.ts
@@ -1,17 +1,20 @@
 import express from 'express';
 import { createProxyMiddleware } from 'http-proxy-middleware';
+import path from 'path';
 
 export const serve = (port: number, filename: string, dir: string) => {
   const app = express();
 
-  app.use(
-    '/',
-    createProxyMiddleware({
-      target: 'http://127.0.0.1:3000',
-      ws: true,
-      logLevel: 'silent',
-    })
-  );
+  const packagePath = require.resolve('coen2/build/index.html');
+  app.use(express.static(path.dirname(packagePath))); 
+  // app.use(
+  //   '/',
+  //   createProxyMiddleware({
+  //     target: 'http://127.0.0.1:3000',
+  //     ws: true,
+  //     logLevel: 'silent',
+  //   })
+  // );
 
   return new Promise<void>((resolve, reject) => {
     app.listen(port, resolve).on('error', reject);
