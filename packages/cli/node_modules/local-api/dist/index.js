0903647016b9f908cc82d17bd8de938914c63e54
Norbert.Duchnowski <dnaid4k@gmail.com>
2023-09-08 13:51:01 +0200

    determin execution environment

--- packages/cli/node_modules/local-api/dist/index.js
+++ packages/cli/node_modules/local-api/dist/index.js
@@ -5,19 +5,21 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.serve = void 0;
 var express_1 = __importDefault(require("express"));
+var http_proxy_middleware_1 = require("http-proxy-middleware");
 var path_1 = __importDefault(require("path"));
-var serve = function (port, filename, dir) {
+var serve = function (port, filename, dir, useProxy) {
     var app = express_1.default();
-    var packagePath = require.resolve('coen2/build/index.html');
-    app.use(express_1.default.static(path_1.default.dirname(packagePath)));
-    // app.use(
-    //   '/',
-    //   createProxyMiddleware({
-    //     target: 'http://127.0.0.1:3000',
-    //     ws: true,
-    //     logLevel: 'silent',
-    //   })
-    // );
+    if (useProxy) {
+        app.use('/', http_proxy_middleware_1.createProxyMiddleware({
+            target: 'http://127.0.0.1:3000',
+            ws: true,
+            logLevel: 'silent',
+        }));
+    }
+    else {
+        var packagePath = require.resolve('coen2/build/index.html');
+        app.use(express_1.default.static(path_1.default.dirname(packagePath)));
+    }
     return new Promise(function (resolve, reject) {
         app.listen(port, resolve).on('error', reject);
     });
